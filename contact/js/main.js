5eaf7667ad92478801348aa4c1006f4938b15bfc
Aliaksei Skakun <skaliaksei@gmail.com>
2021-11-07 16:35:24 +0300

    feat: develop script of map navigation

--- contact/js/main.js
+++ contact/js/main.js
@@ -1,12 +1,37 @@
+let ourMap = document.getElementById('map')
+ourMap.style.backgroundPositionX = `-300px`;
+ourMap.style.backgroundPositionY = `-100px`;
 
-let outX;
-let outY;
-let ourMap = document.getElementById('map');
+let startX = Number(ourMap.style.backgroundPositionX.slice(0, -2));
+let startY = Number(ourMap.style.backgroundPositionY.slice(0, -2));
 
-ourMap.onmousemove = function(event) {
-    outX = String(event.offsetX) + 'px';
-    outY = String(event.offsetY) + 'px';
-    ourMap.style.backgroundPositionX = outX;
-    ourMap.style.backgroundPositionY = outY;
-    console.log(`X: ${event.offsetX}, Y: ${event.offsetY}`)
-}
+ourMap.addEventListener('mousemove', function(event) {
+    if (event.offsetX > ourMap.offsetWidth / 2) {
+
+        let outX = startX + (((ourMap.offsetWidth / 2) - event.offsetX) / 2)
+        outX += 'px'
+        ourMap.style.backgroundPositionX = outX;
+        console.log(outX)
+    } else {
+
+        let outX = startX + (((ourMap.offsetWidth / 2) - event.offsetX) / 2)
+        outX += 'px'
+        ourMap.style.backgroundPositionX = outX;
+        console.log(outX)
+    }
+
+    if (event.offsetY > ourMap.offsetHeight / 2) {
+
+        let outY = startY + ((ourMap.offsetHeight / 2) - event.offsetY) / 2;
+        outY += 'px'
+        ourMap.style.backgroundPositionY = outY;
+        console.log(outY)
+    } else {
+
+        let outY = startY + ((ourMap.offsetHeight / 2) - event.offsetY) / 2;
+        outY += 'px'
+        ourMap.style.backgroundPositionY = outY;
+        console.log(outY)
+    }
+
+});
