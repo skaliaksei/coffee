e3b46bdd64f0222155cdb1b5b271322e7d8040fb
Aliaksei Skakun <skaliaksei@gmail.com>
2021-11-10 22:47:37 +0300

    finish line

--- contact/js/map.js
+++ contact/js/map.js
@@ -1,4 +1,7 @@
+// получение блока с картой
 let ourMap = document.getElementById('map')
+
+// позиционирование центра для разных разрешений экрана
 if (ourMap.offsetWidth > 800) {
     ourMap.style.backgroundPositionX = `-300px`;
 } else if (ourMap.offsetWidth > 600) {
@@ -7,6 +10,8 @@ if (ourMap.offsetWidth > 800) {
     ourMap.style.backgroundPositionX = `-650px`;
 }
 
+// позиционирование бэкграунда через backgroundPosition
+// offsetX и offsetY - слушаем координаты мыши над картой
 
 ourMap.style.backgroundPositionY = `-100px`;
 
@@ -14,6 +19,7 @@ let startX = Number(ourMap.style.backgroundPositionX.slice(0, -2));
 let startY = Number(ourMap.style.backgroundPositionY.slice(0, -2));
 
 ourMap.addEventListener('mousemove', function(event) {
+    // смещение карты по X
     if (event.offsetX > ourMap.offsetWidth / 2) {
 
         let outX = startX + (((ourMap.offsetWidth / 2) - event.offsetX) / 2)
@@ -25,7 +31,7 @@ ourMap.addEventListener('mousemove', function(event) {
         outX += 'px'
         ourMap.style.backgroundPositionX = outX;
     }
-
+    // смещение карты по Y
     if (event.offsetY > ourMap.offsetHeight / 2) {
 
         let outY = startY + ((ourMap.offsetHeight / 2) - event.offsetY) / 2;
