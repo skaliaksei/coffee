7549c2914966d4b6741f6b3336b5ac90f964714d
Aliaksei Skakun <skaliaksei@gmail.com>
2021-11-10 20:39:37 +0300

    script sorting

---
+++ menu/js/sorting.js
@@ -0,0 +1,131 @@
+const allKey = document.getElementById('all');
+const fastFoodKey = document.getElementById('fastFood');
+const coffeeKey = document.getElementById('coffee');
+const coctailsKey = document.getElementById('coctails');
+const cakeKey = document.getElementById('cake');
+const snackKey = document.getElementById('snack');
+
+const cardPlace1 = document.getElementById('menu-card__item1');
+const cardPlace2 = document.getElementById('menu-card__item2');
+const cardPlace3 = document.getElementById('menu-card__item3');
+const cardPlace4 = document.getElementById('menu-card__item4');
+const cardPlace5 = document.getElementById('menu-card__item5');
+const cardPlace6 = document.getElementById('menu-card__item6');
+const cardPlace7 = document.getElementById('menu-card__item7');
+const cardPlace8 = document.getElementById('menu-card__item8');
+const cardPlace9 = document.getElementById('menu-card__item9');
+const cardPlace10 = document.getElementById('menu-card__item10');
+const cardPlace11 = document.getElementById('menu-card__item11');
+const cardPlace12 = document.getElementById('menu-card__item12');
+
+
+allKey.onclick = () => {
+    cardPlace1.classList.remove('show')
+    cardPlace2.classList.remove('show')
+    cardPlace3.classList.remove('show')
+    cardPlace4.classList.remove('show')
+    cardPlace5.classList.remove('show')
+    cardPlace6.classList.remove('show')
+    cardPlace7.classList.remove('show')
+    cardPlace8.classList.remove('show')
+    cardPlace9.classList.remove('show')
+    cardPlace10.classList.remove('show')
+    cardPlace11.classList.remove('show')
+    cardPlace12.classList.remove('show')
+    cardPlace1.classList.add('show')
+    cardPlace2.classList.add('show')
+    cardPlace3.classList.add('show')
+    cardPlace4.classList.add('show')
+    cardPlace5.classList.add('show')
+    cardPlace6.classList.add('show')
+    cardPlace7.classList.add('show')
+    cardPlace8.classList.add('show')
+    cardPlace9.classList.add('show')
+    cardPlace10.classList.add('show')
+    cardPlace11.classList.add('show')
+    cardPlace12.classList.add('show')
+}
+
+fastFoodKey.onclick = () => {
+    cardPlace1.classList.remove('show')
+    cardPlace2.classList.remove('show')
+    cardPlace3.classList.remove('show')
+    cardPlace4.classList.remove('show')
+    cardPlace5.classList.remove('show')
+    cardPlace6.classList.remove('show')
+    cardPlace7.classList.remove('show')
+    cardPlace8.classList.remove('show')
+    cardPlace9.classList.remove('show')
+    cardPlace10.classList.remove('show')
+    cardPlace11.classList.remove('show')
+    cardPlace12.classList.remove('show')
+    cardPlace3.classList.add('show')
+    cardPlace5.classList.add('show')
+}
+
+coffeeKey.onclick = () => {
+    cardPlace1.classList.remove('show')
+    cardPlace2.classList.remove('show')
+    cardPlace3.classList.remove('show')
+    cardPlace4.classList.remove('show')
+    cardPlace5.classList.remove('show')
+    cardPlace6.classList.remove('show')
+    cardPlace7.classList.remove('show')
+    cardPlace8.classList.remove('show')
+    cardPlace9.classList.remove('show')
+    cardPlace10.classList.remove('show')
+    cardPlace11.classList.remove('show')
+    cardPlace12.classList.remove('show')
+    cardPlace1.classList.add('show')
+    cardPlace8.classList.add('show')
+}
+coctailsKey.onclick = () => {
+    cardPlace1.classList.remove('show')
+    cardPlace2.classList.remove('show')
+    cardPlace3.classList.remove('show')
+    cardPlace4.classList.remove('show')
+    cardPlace5.classList.remove('show')
+    cardPlace6.classList.remove('show')
+    cardPlace7.classList.remove('show')
+    cardPlace8.classList.remove('show')
+    cardPlace9.classList.remove('show')
+    cardPlace10.classList.remove('show')
+    cardPlace11.classList.remove('show')
+    cardPlace12.classList.remove('show')
+    cardPlace10.classList.add('show')
+    cardPlace12.classList.add('show')
+}
+cakeKey.onclick = () => {
+    cardPlace1.classList.remove('show')
+    cardPlace2.classList.remove('show')
+    cardPlace3.classList.remove('show')
+    cardPlace4.classList.remove('show')
+    cardPlace5.classList.remove('show')
+    cardPlace6.classList.remove('show')
+    cardPlace7.classList.remove('show')
+    cardPlace8.classList.remove('show')
+    cardPlace9.classList.remove('show')
+    cardPlace10.classList.remove('show')
+    cardPlace11.classList.remove('show')
+    cardPlace12.classList.remove('show')
+    cardPlace2.classList.add('show')
+    cardPlace7.classList.add('show')
+}
+snackKey.onclick = () => {
+    cardPlace1.classList.remove('show')
+    cardPlace2.classList.remove('show')
+    cardPlace3.classList.remove('show')
+    cardPlace4.classList.remove('show')
+    cardPlace5.classList.remove('show')
+    cardPlace6.classList.remove('show')
+    cardPlace7.classList.remove('show')
+    cardPlace8.classList.remove('show')
+    cardPlace9.classList.remove('show')
+    cardPlace10.classList.remove('show')
+    cardPlace11.classList.remove('show')
+    cardPlace12.classList.remove('show')
+    cardPlace5.classList.add('show')
+    cardPlace6.classList.add('show')
+    cardPlace9.classList.add('show')
+    cardPlace11.classList.add('show')
+}
