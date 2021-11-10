9b2e42ae94aaf9d7dda952aad5606e1b5c3a8f2d
Aliaksei Skakun <skaliaksei@gmail.com>
2021-11-10 20:52:16 +0300

    point finish

--- contact/js/map.js
+++ contact/js/map.js
@@ -1,5 +1,4 @@
 let ourMap = document.getElementById('map')
-// console.log(ourMap.offsetWidth);
 if (ourMap.offsetWidth > 800) {
     ourMap.style.backgroundPositionX = `-300px`;
 } else if (ourMap.offsetWidth > 600) {
@@ -34,7 +33,7 @@ ourMap.addEventListener('mousemove', function(event) {
         ourMap.style.backgroundPositionY = outY;
     } else {
 
-        let outY = startY + ((ourMap.offsetHeight / 2) - event.offsetY) / 2;
+        let outY = startY + ((ourMap.offsetHeight / 2) - event.offsetY) / 3;
         outY += 'px'
         ourMap.style.backgroundPositionY = outY;
     }
