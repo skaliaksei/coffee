2209aa76a04626b04230e46997bbebc6f722a2f2
Aliaksei Skakun <skaliaksei@gmail.com>
2021-11-12 20:05:48 +0300

    add fitch- hide switch when mobile menu open

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 if(document.getElementById('burger')) {
     const btn = document.querySelector('.burger-menu');
     const menu = document.querySelector('.mobile-menu');
+    const switchKey = document.querySelector('.dark-key__container');
 
         btn.onclick = function() {
         menu.classList.toggle("mobile-menu--active");
+        switchKey.classList.toggle('key-hide');
     }
 }
